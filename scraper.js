4f598c93c9cfb5805c5b5520146d9fd73b92a0e5
Nick Jordan <cnicholasjordan@gmail.com>
2015-11-10 11:30:32 -0800

    html downloaded using request module.

--- scraper.js
+++ scraper.js
@@ -3,6 +3,9 @@ var http = require('http');
 var request = require('request');
 var cheerio = require('cheerio');
 
-request.get('http://substack.net/images/').on('response', function(response) {
-  console.log(response);
+request.get('http://substack.net/images/', function(error, response) {
+  if (error) {
+    return console.error('failed: ' + error);
+  }
+  console.log(response.body);
 });
